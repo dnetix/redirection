0167f820fc9f0dcabb93e4e03923ef8c4916b917
Diego Calle <dnetix@gmail.com>
2021-09-14 15:54:09 -0500

    Add base settings accessor

--- tests/BaseTestCase.php
+++ tests/BaseTestCase.php
@@ -2,17 +2,28 @@
 
 namespace Tests;
 
+use Dnetix\Redirection\Helpers\Settings;
 use Dnetix\Redirection\PlacetoPay;
 use PHPUnit\Framework\TestCase;
 
 class BaseTestCase extends TestCase
 {
-    public function getGateway($overrides = []): PlacetoPay
+    public function getDefaultSettings(array $overrides = []): array
     {
-        return new PlacetoPay(array_merge([
+        return array_merge([
             'login' => 'not_the_real_login_obviously',
             'tranKey' => 'kXf6FDYdQTH4dhwWs3Ue',
             'url' => 'https://checkout-test.placetopay.com',
-        ], $overrides));
+        ], $overrides);
+    }
+
+    public function getSettings(array $overrides = []): Settings
+    {
+        return new Settings($this->getDefaultSettings($overrides));
+    }
+
+    public function getService(array $overrides = []): PlacetoPay
+    {
+        return new PlacetoPay($this->getDefaultSettings($overrides));
     }
 }
