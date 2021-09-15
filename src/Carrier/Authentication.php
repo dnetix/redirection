6e1cab164e473bd96aaa3282943e1516a9bb4581
Diego Calle <dnetix@gmail.com>
2021-09-14 20:14:56 -0500

    Priorice openssl random bytes

--- src/Carrier/Authentication.php
+++ src/Carrier/Authentication.php
@@ -57,10 +57,10 @@ class Authentication
         if ($this->auth) {
             $nonce = $this->auth['nonce'];
         } else {
-            if (function_exists('random_bytes')) {
-                $nonce = bin2hex(random_bytes(16));
-            } elseif (function_exists('openssl_random_pseudo_bytes')) {
+            if (function_exists('openssl_random_pseudo_bytes')) {
                 $nonce = bin2hex(openssl_random_pseudo_bytes(16));
+            } elseif (function_exists('random_bytes')) {
+                $nonce = bin2hex(random_bytes(16));
             } else {
                 $nonce = mt_rand();
             }
